2422777485808d304d3904500bf1e1e50fe2d53b
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-04-21 14:19:47 +0300

    some setup

--- app/src/main/java/com/nicos/pokedex_compose/MainActivity.kt
+++ app/src/main/java/com/nicos/pokedex_compose/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Pokedex App")
                 }
             }
         }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Pokedex_ComposeTheme {
-        Greeting("Android")
+        Greeting("Pokedex App")
     }
 }
