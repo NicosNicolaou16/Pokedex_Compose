74ce9e3317eeb83ee1cd3feaeaa6008592fcd9cc
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-08-16 01:46:57 +0300

    some changes on UI, support edge to edge

--- app/src/main/java/com/nicos/pokedex_compose/compose/MainActivity.kt
+++ app/src/main/java/com/nicos/pokedex_compose/compose/MainActivity.kt
@@ -2,7 +2,6 @@ package com.nicos.pokedex_compose.compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
