0c5c9d8b266384cfa0315392ca293414ca10a32b
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-02 23:12:30 +0300

    set dark mode as default

--- app/src/main/java/com/nicos/pokedex_compose/presentation/MainActivity.kt
+++ app/src/main/java/com/nicos/pokedex_compose/presentation/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Pokedex_ComposeTheme {
+            Pokedex_ComposeTheme(darkTheme = true) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
