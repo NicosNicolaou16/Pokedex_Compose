d99bb6cb92c4d31f7fd0cceb17a0a9e81ba95d2d
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-02 23:20:06 +0300

    add app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,8 +75,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.swipe.refresh.layout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    //Compose
-    //implementation(libs.androidx.compose.compiler)
+    // Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
@@ -90,33 +89,33 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.animation)
     implementation(libs.androidx.foundation)
-    //Room Database
+    // Room Database
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
-    //Retrofit
+    // Retrofit
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
-    //OkHttp
+    // OkHttp
     implementation(libs.okHttp)
-    //Gson
+    // Gson
     implementation(libs.gson)
-    //Coil
+    // Coil
     implementation(libs.coil)
-    //Coroutines
+    // Coroutines
     implementation(libs.coroutine.core)
     implementation(libs.coroutine.android)
-    //Kotlin Serialization
+    // Kotlin Serialization
     implementation(libs.kotlinx.serialization.json)
-    //Material
+    // Material
     implementation(libs.material)
-    //Hilt
+    // Hilt
     implementation(libs.dagger.android)
     ksp(libs.dagger.compiler)
     ksp(libs.hilt.compiler)
-    //Palette
+    // Palette
     implementation(libs.androidx.palette.ktx)
-    //Unit Test
+    // Unit Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
