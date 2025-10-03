d516fb7bfe4b29c4141f1c613658e4a36e102a27
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-10-04 00:37:18 +0300

    replace the deprecated code

--- app/src/main/java/com/nicos/pokedex_compose/presentation/pokemon_details_screen/PokemonDetailsScreen.kt
+++ app/src/main/java/com/nicos/pokedex_compose/presentation/pokemon_details_screen/PokemonDetailsScreen.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.CachePolicy
 import coil.request.ImageRequest
--- app/src/main/java/com/nicos/pokedex_compose/presentation/pokemon_list_screen/PokemonListScreen.kt
+++ app/src/main/java/com/nicos/pokedex_compose/presentation/pokemon_list_screen/PokemonListScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.CachePolicy
