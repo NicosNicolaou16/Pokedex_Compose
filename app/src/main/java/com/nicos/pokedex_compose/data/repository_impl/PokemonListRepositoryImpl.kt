cb253556e882fb12a80ed4563887cf34f33f52e7
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-10-04 00:46:53 +0300

    apply DTO

--- app/src/main/java/com/nicos/pokedex_compose/data/repository_impl/PokemonListRepositoryImpl.kt
+++ app/src/main/java/com/nicos/pokedex_compose/data/repository_impl/PokemonListRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.nicos.pokedex_compose.data.repository_impl
 
+import androidx.core.text.isDigitsOnly
 import com.nicos.pokedex_compose.data.room_database.entities.PokemonEntity
 import com.nicos.pokedex_compose.data.room_database.init_database.MyRoomDatabase
 import com.nicos.pokedex_compose.domain.network.PokemonService
@@ -8,7 +9,6 @@ import com.nicos.pokedex_compose.utils.generic_classes.HandlingError
 import com.nicos.pokedex_compose.utils.generic_classes.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
@@ -18,6 +18,12 @@ class PokemonListRepositoryImpl @Inject constructor(
     private val pokemonService: PokemonService,
     private val handlingError: HandlingError,
 ) : PokemonListRepository {
+
+    companion object {
+        private const val BASE_IMAGE_URL =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/"
+        private const val PNG_FORMAT = ".png"
+    }
 
     override suspend fun fetchPokemonList(url: String?): Flow<Resource<MutableList<PokemonEntity>>> {
         return flow {
@@ -40,11 +46,23 @@ class PokemonListRepositoryImpl @Inject constructor(
         }.flowOn(Dispatchers.IO)
     }
 
-    override suspend fun savePokemon(pokemonEntityList: MutableList<PokemonEntity>) =
-        PokemonEntity.savePokemonList(
-            pokemonEntityList = pokemonEntityList,
-            myRoomDatabase = myRoomDatabase
-        ).collect()
+    override suspend fun savePokemon(pokemonEntityList: MutableList<PokemonEntity>) {
+        pokemonEntityList.forEach {
+            buildPokemonImageUrl(it)
+            if (it.imageUrl != null) {
+                myRoomDatabase.pokemonDao().insertOrReplaceObject(it)
+            }
+        }
+    }
+
+    private fun buildPokemonImageUrl(pokemonEntity: PokemonEntity) {
+        val pokemonIdAsString =
+            pokemonEntity.url?.substringBeforeLast("/")?.substringAfterLast("/")
+        if (pokemonIdAsString != null && pokemonIdAsString.isDigitsOnly()) {
+            pokemonEntity.order = pokemonIdAsString.toInt()
+            pokemonEntity.imageUrl = "${BASE_IMAGE_URL}$pokemonIdAsString${PNG_FORMAT}"
+        }
+    }
 
     override suspend fun offline(): Flow<Resource<MutableList<PokemonEntity>>> {
         return flow {
