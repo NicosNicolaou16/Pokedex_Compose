7a39cb03116db4cc1572be2d2fdd0c872d9b9cd9
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-12-13 00:38:08 +0200

    improve the state for nav3

--- app/src/main/java/com/nicos/pokedex_compose/presentation/navigation/navigation_3/NavigationState.kt
+++ app/src/main/java/com/nicos/pokedex_compose/presentation/navigation/navigation_3/NavigationState.kt
@@ -1,10 +1,11 @@
 package com.nicos.pokedex_compose.presentation.navigation.navigation_3
 
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 
 class NavigationState(
-    val backStacks: SnapshotStateList<NavKey>
+    val backStacks: NavBackStack<NavKey>
 ) {
     val stacksInUse: SnapshotStateList<NavKey>
         get() = backStacks
