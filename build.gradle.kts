17a8d53a99484c95c3aa8589466413c2b77bc90c
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-25 00:13:25 +0200

    remove Kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.parcelize) apply false
