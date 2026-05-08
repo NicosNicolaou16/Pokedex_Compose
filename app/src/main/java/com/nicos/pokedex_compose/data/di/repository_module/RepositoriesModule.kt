9ae96f0c598ba7133a3f0d42dc9c15af1644ff0b
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-05-08 16:23:00 +0300

    fix architecture stracture

--- app/src/main/java/com/nicos/pokedex_compose/data/di/repository_module/RepositoriesModule.kt
+++ app/src/main/java/com/nicos/pokedex_compose/data/di/repository_module/RepositoriesModule.kt
@@ -2,8 +2,10 @@ package com.nicos.pokedex_compose.data.di.repository_module
 
 import com.nicos.pokedex_compose.data.network.PokemonService
 import com.nicos.pokedex_compose.data.repository_impl.PokemonDetailsRepositoryImpl
+import com.nicos.pokedex_compose.data.repository_impl.PokemonListRepositoryImpl
 import com.nicos.pokedex_compose.data.room_database.init_database.MyRoomDatabase
 import com.nicos.pokedex_compose.domain.repositories.PokemonDetailsRepository
+import com.nicos.pokedex_compose.domain.repositories.PokemonListRepository
 import com.nicos.pokedex_compose.utils.generic_classes.HandlingError
 import dagger.Module
 import dagger.Provides
@@ -13,6 +15,19 @@ import dagger.hilt.android.components.ViewModelComponent
 @Module
 @InstallIn(ViewModelComponent::class)
 object RepositoriesModule {
+
+    @Provides
+    fun getPokemonListRepository(
+        myRoomDatabase: MyRoomDatabase,
+        pokemonService: PokemonService,
+        handlingError: HandlingError
+    ): PokemonListRepository {
+        return PokemonListRepositoryImpl(
+            myRoomDatabase,
+            pokemonService,
+            handlingError
+        )
+    }
 
     @Provides
     fun getPokemonDetailsRepository(
