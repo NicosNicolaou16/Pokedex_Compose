dd61a5733ee2204e1be00d39dc4838c609fc2f0d
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-07-05 01:19:33 +0300

    migrate to Room 3

--- app/src/main/java/com/nicos/pokedex_compose/data/room_database/entities/PokemonDetailsWithStatsEntity.kt
+++ app/src/main/java/com/nicos/pokedex_compose/data/room_database/entities/PokemonDetailsWithStatsEntity.kt
@@ -1,16 +1,16 @@
 package com.nicos.pokedex_compose.data.room_database.entities
 
-import androidx.room.Embedded
-import androidx.room.Relation
+import androidx.room3.Embedded
+import androidx.room3.Relation
 
 /**
- * One to Many relationship between PokemonDetailsEntity and StatsEntity
+ * One-to-Many relationship between PokemonDetailsEntity and StatsEntity
  * */
 data class PokemonDetailsWithStatsEntity(
     @Embedded val pokemonDetailsEntity: PokemonDetailsEntity,
     @Relation(
-        parentColumn = "name",
-        entityColumn = "pokemonName"
+        parentColumns = ["name"],
+        entityColumns = ["pokemonName"]
     )
     val statsEntityList: List<StatsEntity>
 )
